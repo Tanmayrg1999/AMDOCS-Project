c55b4c1fcf7dc30ec39f52d98b138cc5c9a87ae3
Tanmay Ravindra Gaurkar <53641559+Tanmayrg1999@users.noreply.github.com>
2021-07-25 15:00:46 +0530

    Create Course.java

---
+++ src/com/training/model/Course.java
@@ -0,0 +1,25 @@
+package com.training.model;
+
+public class Course {
+	private String c_name;
+	private String c_desp;
+	private String c_fees;
+	public String getC_name() {
+		return c_name;
+	}
+	public void setC_name(String c_name) {
+		this.c_name = c_name;
+	}
+	public String getC_desp() {
+		return c_desp;
+	}
+	public void setC_desp(String c_desp) {
+		this.c_desp = c_desp;
+	}
+	public String getC_fees() {
+		return c_fees;
+	}
+	public void setC_fees(String c_fees) {
+		this.c_fees = c_fees;
+	}
+}
