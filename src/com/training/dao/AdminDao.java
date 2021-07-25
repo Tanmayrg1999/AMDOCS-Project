750236381339b38fdc1a672af3c19b902bb31c25
Tanmay Ravindra Gaurkar <53641559+Tanmayrg1999@users.noreply.github.com>
2021-07-25 14:53:11 +0530

    Create AdminDao.java

---
+++ src/com/training/dao/AdminDao.java
@@ -0,0 +1,55 @@
+package com.training.dao;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.training.model.Student;
+
+public class AdminDao {
+
+    public boolean validate(Student loginBean) throws ClassNotFoundException {
+        boolean status = false;
+
+        Class.forName("com.mysql.jdbc.Driver");
+
+        try  {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@//localhost:1521/orcl","sys as sysdba","system");
+            // Step 2:Create a statement using connection object
+            //CallableStatement stmt=connection.prepareCall("{?= call getId(?)}");
+            PreparedStatement preparedStatement = connection
+            .prepareStatement("select * from admin where username = ? and password = ? ");
+            preparedStatement.setString(1, loginBean.getusername());
+            preparedStatement.setString(2, loginBean.getpassword());
+
+            System.out.println(preparedStatement);
+            ResultSet rs = preparedStatement.executeQuery();
+            status = rs.next();
+
+        } catch (SQLException e) {
+            // process sql exception
+            printSQLException(e);
+        }
+        return status;
+    }
+
+    private void printSQLException(SQLException ex) {
+        for (Throwable e: ex) {
+            if (e instanceof SQLException) {
+                e.printStackTrace(System.err);
+                System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+                System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
+                System.err.println("Message: " + e.getMessage());
+                Throwable t = ex.getCause();
+                while (t != null) {
+                    System.out.println("Cause: " + t);
+                    t = t.getCause();
+                }
+            }
+        }
+    }
+}
