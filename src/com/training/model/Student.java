4a1492ea14d7b743d90da2a3b63d344046db4876
Tanmay Ravindra Gaurkar <53641559+Tanmayrg1999@users.noreply.github.com>
2021-07-25 15:01:32 +0530

    Add files via upload

---
+++ src/com/training/model/Student.java
@@ -0,0 +1,46 @@
+package com.training.model;
+
+public class Student {
+	private String firstname;
+	private String lastname;
+	private String username;
+	private String password;
+	private String address;
+	private String contact;
+	public String getfirstname() {
+		return firstname;
+	}
+	public void setfirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getlastname() {
+		return lastname;
+	}
+	public void setlastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getusername() {
+		return username;
+	}
+	public void setusername(String username) {
+		this.username = username;
+	}
+	public String getpassword() {
+		return password;
+	}
+	public void setpassword(String password) {
+		this.password = password;
+	}
+	public String getaddress() {
+		return address;
+	}
+	public void setaddress(String address) {
+		this.address = address;
+	}
+	public String getcontact() {
+		return contact;
+	}
+	public void setcontact(String contact) {
+		this.contact = contact;
+	}
+}
